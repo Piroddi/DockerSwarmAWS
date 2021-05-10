8fbcfb02b48b896e3797d8c25fc2a21605cb296d
Luke Mwila <outlier.developer@gmail.com>
2021-05-10 14:34:24 +0200

    feature: added vpc endpoints for ecr and s3

--- Terraform/modules/network/vpc_endpoints.tf
+++ Terraform/modules/network/vpc_endpoints.tf
@@ -15,3 +15,32 @@ resource "aws_vpc_endpoint" "ssm-messages" {
   security_group_ids = [module.vpc.default_security_group_id]
   subnet_ids = module.vpc.private_subnets
 }
+
+# ECR
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id       = module.vpc.vpc_id
+  service_name = "com.amazonaws.eu-west-1.ecr.dkr"
+  vpc_endpoint_type = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = module.vpc.private_subnets
+
+  security_group_ids = [module.vpc.default_security_group_id]
+}
+
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id       = module.vpc.vpc_id
+  service_name = "com.amazonaws.eu-west-1.ecr.api"
+  vpc_endpoint_type = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = module.vpc.private_subnets
+
+  security_group_ids = [module.vpc.default_security_group_id]
+}
+
+# S3
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = module.vpc.vpc_id
+  service_name = "com.amazonaws.eu-west-1.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids = [module.vpc.default_route_table_id]
+}
